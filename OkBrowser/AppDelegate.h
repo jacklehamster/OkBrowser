a9b3d21c9d94a7332081be9c73c8f8012ff3abcb
vlequang <vlequang@adobe.com>
2013-08-27 15:15:55 -0700

    Initial Commit

---
+++ OkBrowser/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OkBrowser
+//
+//  Created by Vincent Le Quang on 8/27/13.
+//  Copyright (c) 2013 Jack Le Hamster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *mainViewController;
+
+@end
---
+++ OkBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OkBrowser
+//
+//  Created by Vincent Le Quang on 8/27/13.
+//  Copyright (c) 2013 Jack Le Hamster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
